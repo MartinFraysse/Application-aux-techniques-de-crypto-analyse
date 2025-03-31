6a3692587e0ce411d8205a5043d1439fefbceda8
MartinFraysse <martinfraysse75@gmail.com>
2025-03-31 12:24:42 +0200

    t

--- question3.3/test_send.py
+++ question3.3/test_send.py
@@ -8,7 +8,7 @@ def test(mdp: str):
     try:
         with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
             # Envoyer le mot de passe candidat
-            ser.write(mdp.encode() + b'\n')
+            ser.write('U ' + mdp.encode() + b'\n')
             # Attendre la réponse (suppose que la cible renvoie '1' pour vrai, '0' pour faux)
             response = ser.readline().decode().strip()
             print(response)
